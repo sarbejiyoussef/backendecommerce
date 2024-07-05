87af42cc1b39d3ff5be2698325b244c69a621442
sarbejiyoussef <sarbejiyoussef@gmail.com>
2024-07-05 20:55:03 +0100

    Commit new version

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const mongoose =require("mongoose")
+const dotenv =require('dotenv')
+const app = express();
+const categorieRouter=require("./routes/categorie.route")
+const scategorieRouter =require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
+//config dotenv
+dotenv.config()
+
+//BodyParser Middleware
+app.use(express.json());
+// Connexion à la base données
+mongoose.connect(process.env.DATABASE)
+.then(() => {console.log("DataBase Successfully Connected");})
+.catch(err => { console.log("Unable to connect to database", err);
+process.exit(); });
+// requête
+app.get("/",(req,res)=>{
+res.send("bonjour");});
+
+
+app.use("/api/categories",categorieRouter)
+app.use('/api/scategories', scategorieRouter);
+app.use('/api/articles', articleRouter);
+app.listen(process.env.PORT, () => {
+console.log(`Server is listening on port ${process.env.PORT}`); });
+module.exports = app;
